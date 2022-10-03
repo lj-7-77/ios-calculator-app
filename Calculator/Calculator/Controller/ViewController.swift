a4b4f109fdaa512370d3b7c7fdedc6849ab6e1c3
lj-7-77 <swtw72@gmail.com>
2022-10-03 18:11:38 +0900

    feat: 숫자버튼 액션함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -5,16 +5,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var inputNumberArray: [String] = []
+    var displayResultValue: String = ""
     
     @IBOutlet weak var signLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
     
     @IBAction func touchNumber(_ sender: UIButton) {
+        inputNumberArray.append(sender.currentTitle ?? "")
         
+        if inputNumberArray.count == 1 && inputNumberArray[0] == "."  {
+            inputNumberArray.insert("0", at: 0)
+        } else if inputNumberArray.count == 2 {
+            guard inputNumberArray[1] != "." else {
+                numberLabel.text = inputNumberArray.joined()
+                return
+            }
+            switch inputNumberArray[0] {
+            case "0":
+                inputNumberArray.removeFirst()
+            case ".":
+                inputNumberArray.insert("0", at: 0)
+            default:
+                break
+            }
+        } else if inputNumberArray.count > 2 {
+            if inputNumberArray[inputNumberArray.count-1] == "."
+                && inputNumberArray.contains(".") {
+                inputNumberArray.removeLast()
+            }
+        }
+        numberLabel.text = inputNumberArray.joined()
     }
     
     @IBAction func touchOperator(_ sender: UIButton) {
-        
+
     }
     
     override func viewDidLoad() {
