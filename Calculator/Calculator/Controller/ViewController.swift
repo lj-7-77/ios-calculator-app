220d45bf296bd11162252df40a83a60b4142d4e9
lj-7-77 <swtw72@gmail.com>
2022-10-03 18:02:35 +0900

    feat: IBOutlet, IBAction 추가, 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -5,6 +5,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var signLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func touchNumber(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchOperator(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
